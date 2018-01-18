109a11ab72963c32a18d622c297e54f69cbc4ebe
Jon Moses <jon@burningbush.us>
2018-01-18 17:24:54 -0500

    Remove slack-redir WIP

--- webview.js
+++ webview.js
@@ -1,34 +1,46 @@
-import { ipcRenderer } from 'electron';
-import path from 'path';
+'use strict';
+
+var _electron = require('electron');
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const getTeamIcon = function getTeamIcon(count = 0) {
   let countTeamIconCheck = count;
   let bgUrl = null;
 
-  document.querySelector('#team_menu').click();
-  countTeamIconCheck += 1;
-  const icon = document.querySelector('.team_icon');
-  if (icon) {
-    bgUrl = window.getComputedStyle(icon, null).getPropertyValue('background-image');
-    bgUrl = /^url\((['"]?)(.*)\1\)$/.exec(bgUrl);
-    bgUrl = bgUrl ? bgUrl[2] : '';
+  const teamMenu = document.querySelector('#team_menu');
+  if (teamMenu) {
+    teamMenu.click();
+
+    const icon = document.querySelector('.team_icon');
+    if (icon) {
+      bgUrl = window.getComputedStyle(icon, null).getPropertyValue('background-image');
+      bgUrl = /^url\((['"]?)(.*)\1\)$/.exec(bgUrl);
+      bgUrl = bgUrl ? bgUrl[2] : '';
+    }
+
+    setTimeout(() => {
+      document.querySelector('.team_menu').remove();
+      document.querySelector('#msg_input .ql-editor').focus();
+    }, 10);
   }
+
+  countTeamIconCheck += 1;
 
   if (bgUrl) {
-    ipcRenderer.sendToHost('avatar', bgUrl);
-  } else if (countTeamIconCheck <= 3) {
+    _electron.ipcRenderer.sendToHost('avatar', bgUrl);
+  } else if (countTeamIconCheck <= 5) {
     setTimeout(() => {
       getTeamIcon(countTeamIconCheck + 1);
-    }, 1000);
+    }, 2000);
   }
-
-  setTimeout(() => {
-    document.querySelector('.team_menu').remove();
-    document.querySelector('#msg_input .ql-editor').focus();
-  }, 10);
 };
 
-module.exports = (Franz) => {
+module.exports = Franz => {
   const getMessages = () => {
     const directMessages = document.querySelectorAll('.p-channel_sidebar__badge').length;
     const allMessages = document.querySelectorAll('.p-channel_sidebar__channel--unread:not([class*="p-channel_sidebar__channel--muted"])').length - directMessages;
@@ -38,10 +50,26 @@ module.exports = (Franz) => {
   };
   Franz.loop(getMessages);
 
+  // remove the slack redirection host from external urls,
+  // like the native Slack.app does.
+  const replaceUrls = () => {
+    var links = document.querySelectorAll('a[href^="https://slack-redir"]'),
+        i = links.length;
+
+    while(i--) {
+      var link = links[i];
+      link.role = '_link';
+      link.href = decodeURIComponent(link.href.split("=")[1]);
+      link.parentNode.replaceChild(link.cloneNode(true), link)
+    }
+    
+  };
+  Franz.loop(replaceUrls);
+
   setTimeout(() => {
     getTeamIcon();
   }, 4000);
 
   // inject franz.css stylesheet
-  Franz.injectCSS(path.join(__dirname, 'service.css'));
+  Franz.injectCSS(_path2.default.join(__dirname, 'service.css'));
 };
